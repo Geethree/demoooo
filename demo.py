a29ac7cf8f809411d5e92b29a633fcea83ef7a85
geethree <gerald.spencer@strateos.com>
2020-12-11 13:48:26 -0800

    demo 'works'

---
+++ demo.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+import github3
+
+
+class SyncR:
+	def __init__(self, user, passs, repository):
+		self.login_to_gh(user, passs)
+
+
+	def login_to_gh(self, user, passs):
+		github3.login(username=user, password=passs)
+
+	def checkout_current_main(self,):
+		pass
+
+	def check_for_local_changes(self,):
+		pass
+
+	def create_branch(self,):
+		pass
+
+	def commit(self,):
+		pass
+
+	def push_to_remote(self,):
+		pass
+
+	def create_pr():
+		pass
+
+
+if __name__== "__main__":
+	repository = sys.argv[1]
+	username = sys.argv[2]
+	password = sys.argv[3]
+
+	syncr = SyncR(username, password, repository)
+
+	syncr.checkout_current_main()
+	
+	if(syncr.check_for_local_changes()):
+		#if we have changes
+		syncr.create_branch()
+		syncr.commit()
+		syncr.push_to_remote()
+		syncr.create_pr()
+	else:
+		pass # do something else mayybe log to syslog
+
+
